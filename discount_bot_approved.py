8e641fecde2f6b188c469785966c7a82ce868000
mandari1ne <marina.stakhovets@gmail.com>
2025-05-08 12:39:11 +0300

    getting info from sheet

--- discount_bot_approved.py
+++ discount_bot_approved.py
@@ -9,4 +9,28 @@ GOOGLE_JSON = r'**.json'
 SHEET_URL = '**'
 
 
+# getting info from sheet
+def get_info_from_sheet() -> dict:
+    '''
+    :return: Dictionary (list of shops by category)
 
+    Getting information from google sheet
+    and adding it to dictionary for convenience of work.
+    '''
+
+    gc = gspread.service_account(filename=GOOGLE_JSON)
+    sh = gc.open_by_url(SHEET_URL)
+    wrksht = sh.get_worksheet(0)
+    sheet_data = wrksht.get_all_values()
+
+    shops = {}
+    for row in sheet_data[1:]:
+        category = row[8]
+        shop = row[0]
+
+        if category not in shops:
+            shops[category] = [shop]
+        else:
+            shops[category].append(shop)
+
+    return shops
